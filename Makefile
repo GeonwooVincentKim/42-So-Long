1467259ceb0b3ef24c6dcee067462b26b3672778
Geonwoo Kim <geonwkim@c2r8s5.42tokyo.jp>
2024-07-03 19:07:17 +0900

    test

--- Makefile
+++ Makefile
@@ -1,75 +1,35 @@
-# SRCS 	= main.c
-# OBJS	= $(SRCS:.c=.o)
+NAME = so_long
+SOURCES = so_long.c get_next_line.c validate.c setup.c gameplay.c
+OBJECTS = $(SOURCES:.c=.o)
 
-# NAME	= so_long
-# CC		= cc
-# RM		= rm -f
-# CFLAGS	= -Wall -Wextra	-Werror
-# LIBFT	= ./libft/libft.a
-# MINILIBX = ./minilibx_opengl/libmlx.a
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
 
-# INCLUDES = -I/usr/include -Imlx
- 
-# MLX_DIR = ./mlx
-# MLX_LIB = $(MLX_DIR)/libmlx_$(UNAME).a
-# MLX_FLAGS = -Lmlx -lmlx -framework OpenGL -framework AppkKit
-
-# %.o:%.c
-# 	$(CC) $(CFLAGS) -c $< -o $@
-
-# $(NAME): $(OBJS)
-# 	$(MAKE) -C ./libft
-# 	$(MAKE) -C ./minilibx_opengl
-# 	cp libft/libft.a $(NAME)
-# 	cp minilibx_opengl/libmlx.a $(NAME)
-# 	ar -rcs $(NAME) $(OBJS)
-
-# all: $(NAME)
-
-# clean:
-# 	$(MAKE) clean -C ./libft
-# 	$(MAKE) clean -C ./minilibx_opengl
-# 	$(RM) $(OBJS)
-
-# fclean: clean
-# 	$(MAKE) fclean -C ./libft
-# 	$(RM) $(NAME)
-
-# re: fclean	all
-
-# .PHONY: all clean fclean re
-NAME=so_long
-CFLAGS=-Wall -Wextra -Werror
-SRCS=main.c
-OBJS=$(SRCS:.c=.o)
-
-MINILIBX_DIR := ./minilibx_opengl
-MINILIBX := $(MINILIBX_DIR)/libmlx.a
-
-INCLUDES := -I$(MINILIBX_DIR)
-LDFLAGS := -L$(MINILIBX_DIR)
-LIBS=-lm -lmlx
-
-.PHONY: all
 all: $(NAME)
 
-$(NAME): $(OBJS) $(MINILIBX)
-	$(CC) $(OBJS) $(LDFLAGS) $(LIBS) -framework OpenGL -framework AppKit -o $(NAME)
+bonus: $(NAME)
+
+$(NAME): $(OBJECTS) minilibx libft
+	gcc -o $@ $(OBJECTS) -Lminilibx_opengl_20191021 -lmlx -Llibft -lft \
+		-framework OpenGL -framework AppKit -lz
 
 %.o: %.c
-	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+	$(CC) -c $(CFLAGS) $?
 
-$(MINILIBX):
-	$(MAKE) -j4 -C $(MINILIBX_DIR)
+minilibx:
+	make -C minilibx_opengl_20191021
 
-.PHONY: clean
+libft:
+	make -C libft
+
 clean:
-	$(RM) $(OBJS)
+	rm -f $(OBJECTS)
+	make -C minilibx_opengl_20191021 clean
+	make -C libft clean
 
-.PHONY: fclean
 fclean: clean
-	$(RM) $(NAME)
-	$(MAKE) -C $(MINILIBX_DIR) clean
+	rm -f $(NAME) libft/libft.a
 
-.PHONY: re
 re: fclean all
+
+.PHONY: all bonus minilibx libft clean fclean re
---
+++ validate.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 19:03:42 by geonwkim          #+#    #+#             */
+/*   Updated: 2024/07/03 19:03:45 by geonwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include "so_long.h"
+#include "libft/libft.h"
+
+static int	char_count(char *line, int c)
+{
+	int	ret;
+
+	ret = 0;
+	while (*line)
+		if (*line++ == c)
+			++ret;
+	return (ret);
+}
+
+static int	is_set(char *line, char *set)
+{
+	int			i;
+	char		*tmp;
+	const int	len = ft_strlen(line);
+
+	i = 0;
+	while (*line)
+	{
+		tmp = set;
+		while (*tmp)
+			if (*line == *tmp++)
+				++i;
+		++line;
+	}
+	if (i != len)
+		return (0);
+	return (1);
+}
+
+static int	is_ber(char *file)
+{
+	const char	*ext = ft_strrchr(file, '.');
+	const int	len = ft_strlen(ext);
+
+	if (!ft_strncmp(ext, ".ber", len))
+		return (1);
+	return (0);
+}
+
+static int	is_rectangular_and_cep(char *file, t_map *map)
+{
+	int			i;
+	char		*line;
+	const int	fd = open(file, O_RDONLY);
+
+	i = 0;
+	line = get_next_line(fd);
+	while (line && ++i)
+	{
+		if (i == 1)
+			map->cols = ft_strlen(line);
+		else if ((int)ft_strlen(line) != map->cols)
+			map->diff_cols = 1;
+		map->c += char_count(line, 'C');
+		map->e += char_count(line, 'E');
+		map->p += char_count(line, 'P');
+		map->v += char_count(line, 'V');
+		free(line);
+		line = get_next_line(fd);
+	}
+	map->rows = i;
+	if (!map->c || !map->e || map->p != 1 || map->diff_cols)
+		return (0);
+	return (1);
+}
+
+int	is_valid_map(char *file, t_map *map)
+{
+	int			i;
+	int			is_valid;
+	char		*line;
+	const int	fd = open(file, O_RDONLY);
+
+	is_valid = 1;
+	if (!is_ber(file) || !is_rectangular_and_cep(file, map))
+		is_valid = 0;
+	i = 0;
+	line = get_next_line(fd);
+	while (line && ++i)
+	{
+		if ((i == 1 || i == map->rows) && !is_set(line, "1"))
+			is_valid = 0;
+		else if (i != 1 && i != map->rows && (!is_set(line, "01CEPV") \
+				|| line[0] != '1' || line[map->cols - 1] != '1'))
+			is_valid = 0;
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (!is_valid)
+		return (0);
+	return (1);
+}
