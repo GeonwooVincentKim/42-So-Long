fd36a5fed8926d5990fb92e905782406d1048a3d
Geonwoo Kim <geonwkim@c2r8s5.42tokyo.jp>
2024-07-03 20:16:48 +0900

    test

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 01:22:20 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/04/25 19:07:50 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/07/03 20:16:36 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,14 @@ static void	del(void *content)
 	free(content);
 }
 
-int main()
-{
-    t_list *list = ft_lstnew(strdup("One"));
-    ft_lstadd_back(&list, ft_lstnew(strdup("Two")));
-    ft_lstadd_back(&list, ft_lstnew(strdup("Three")));
-    ft_lstclear(&list, del);
-    ft_lstclear(&list, del);
-	printf("%lu", (unsigned long)list);
-    printf("List cleared.\n");
-    return 0;
-}
+// int main()
+// {
+//     t_list *list = ft_lstnew(strdup("One"));
+//     ft_lstadd_back(&list, ft_lstnew(strdup("Two")));
+//     ft_lstadd_back(&list, ft_lstnew(strdup("Three")));
+//     ft_lstclear(&list, del);
+//     ft_lstclear(&list, del);
+// 	printf("%lu", (unsigned long)list);
+//     printf("List cleared.\n");
+//     return 0;
+// }
